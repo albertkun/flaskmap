b6be3971f36fb1daacbf1249090148be94d9bd6a
albertkun <albertk@gmx.com>
2018-02-06 03:27:15 -0800

    minor updates

--- 03_geocoder.py
+++ 03_geocoder.py
@@ -4,6 +4,7 @@ from models import *
 
 for event in Event.select().where(Event.lat <>''):
     print event
+    sleep(1)
     try:
         # Form the URL with the address in it
         url = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address={}".format(event.full_address())
@@ -16,7 +17,7 @@ for event in Event.select().where(Event.lat <>''):
         # {u'lat': 40.7135296, u'lng': -73.9856844}
         coords = response.json()['results'][0]['geometry']['location']
 
-        # Assign the lat/lng into the school object (the row)
+        # Assign the lat/lng into the object (the row)
         event.lat = coords['lat']
         event.lon = coords['lng']
 
