58e7a6beea87a031e107dbc53f1be5122375cc3a
albertkun <albertk@gmx.com>
2018-02-07 03:17:16 -0800

    jane map alpha 0.6
    almost done, added web map with points from postgresql, next steps:
    
    1) add form to post data
    2) create rest API
    3) clean up css

--- app.py
+++ app.py
@@ -3,18 +3,25 @@ from flask import Flask, render_template
 from models import *
 
 app = Flask(__name__)
-
+def basicQuery():
+  global events
+  events = Event.select().order_by(Event.date.asc())
+  
+  
 @app.route('/')
 def index():
-  count = Event.select().count
-  events = Event.select().order_by(Event.date.asc())
+  basicQuery()
   return render_template('index.html',events=events)
 
 @app.route('/events/<uid>')
 def uid(uid=None):
     events = Event.select().where(Event.uid == uid)
-    #return render_template('event.html', events=events)
-    return render_template('map.html', events=events)  
+    return render_template('event.html', events=events)  
+
+@app.route('/info')
+def info():
+  basicQuery()
+  return render_template('info.html', events=events)  
 
 if __name__ == '__main__':
     app.run(debug=True)
