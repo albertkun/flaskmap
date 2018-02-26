a03a093a5225f5e0dc76031da6cbc58b2e1d244b
Yoh Kawano <yohkawano@dhcp14-223.wireless.ats.ucla.edu>
2018-02-26 12:25:33 -0800

    v3.0 spanking new look!

--- app.py
+++ app.py
@@ -15,31 +15,43 @@ from models import *
 
 ### api schema begins here ###
 class bookingSchema(ma.Schema):
-    class Meta:
-        # Fields to expose through api
-        fields = ('fid', 'desc_', 'slug','sex','charge','age','home_addre','x','y')
-        json_module = simplejson
+	class Meta:
+		# Fields to expose through api
+		# fields = ('fid', 'desc_', 'slug','sex','home_addre','x','y')
+		fields = ('booking_nu','sex','race','charge','charge_des','charge_lev','bail','year','longitude','latitude','slug')
+		json_module = simplejson
 
 booking_schema = bookingSchema()
 bookings_schema = bookingSchema(many=True)
 
+class raceSchema(ma.Schema):
+	class Meta:
+		# Fields to expose through api
+		# fields = ('fid', 'desc_', 'slug','sex','home_addre','x','y')
+		fields = ('race','count')
+		json_module = simplejson
+
+race_schema = raceSchema()
+races_schema = raceSchema(many=True)
+
 ### api schema ends here ###
 
-def basicQuery():
-	global bookings
-	global neighborhoods
+# def basicQuery():
+# 	global bookings
+# 	global neighborhoods
 
-	bookings = lapd.select()
-    # neighborhoods = lapd.select(lapd.slug).distinct().order_by(lapd.slug.asc())
-	neighborhoods = lapd.select(lapd.slug).distinct().order_by(lapd.slug.asc())
-	# bookings = lapd.select().where(lapd.slug == "downtown")
+# 	bookings = lapd.select()
+#     # neighborhoods = lapd.select(lapd.slug).distinct().order_by(lapd.slug.asc())
+# 	neighborhoods = lapd.select(lapd.slug).distinct().order_by(lapd.slug.asc())
+# 	# bookings = lapd.select().where(lapd.slug == "downtown")
 
 @app.route('/')
 def index():
-	basicQuery()
-	bookings = lapd.select().where(lapd.slug == "downtown")
-	count = lapd.select().where(lapd.slug == "downtown").count()
-	return render_template('index.html',bookings=bookings,count=count,neighborhoods = neighborhoods)
+	# basicQuery()
+	neighborhoods = lasd.select(lasd.slug).distinct().order_by(lasd.slug.asc())
+	return render_template('index.html',neighborhoods = neighborhoods)
+	# bookings = lapd.select().where(lapd.slug == "downtown")
+	# count = lapd.select().where(lapd.slug == "downtown").count()
 
 # @app.route('/<slug>')
 # def slug(slug=None):
@@ -48,11 +60,25 @@ def index():
 # 	count = lapd.select().where(lapd.slug == slug).count()
 # 	return render_template('index.html',bookings=bookings,count=count,neighborhoods = neighborhoods)
 
+# @app.route("/<slug>/<year>/bookings/", methods=['GET'])
+# def get_bookings(slug,year):
+# 	all_bookings = lasd.select().where(lasd.slug == slug,lasd.year==year)
+# 	# all_bookings = lasd.select().where(lasd.slug == slug)
+# 	result = bookings_schema.dump(all_bookings)
+# 	return jsonify(result.data)
 @app.route("/<slug>/bookings/", methods=['GET'])
-def get_events(slug):
-    all_bookings = lapd.select().where(lapd.slug == slug)
-    result = bookings_schema.dump(all_bookings)
-    return jsonify(result.data)
+def get_bookings(slug):
+	all_bookings = lasd.select().where(lasd.slug == slug)
+	# all_bookings = lasd.select().where(lasd.slug == slug)
+	result = bookings_schema.dump(all_bookings)
+	return jsonify(result.data)
+
+@app.route("/<slug>/<year>/race/", methods=['GET'])
+def get_race(slug,year):
+	all_bookings = lasd.select(lasd.race,fn.COUNT(lasd.race)).where(lasd.slug == slug,lasd.year==year).group_by(lasd.race)
+	# all_bookings = lasd.select().where(lasd.slug == slug)
+	result = races_schema.dump(all_bookings)
+	return jsonify(result.data)
 
 
 ### not needed anymore ###
@@ -68,9 +94,9 @@ def get_events(slug):
 
 @app.route('/_add_numbers')
 def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+	a = request.args.get('a', 0, type=int)
+	b = request.args.get('b', 0, type=int)
+	return jsonify(result=a + b)
 
 # @app.route('/info')
 # def info():
@@ -78,4 +104,4 @@ def add_numbers():
 #   return render_template('info.html', events=events)  
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
--- models.py
+++ models.py
@@ -4,25 +4,49 @@ from peewee import *
 # Connect to the PostgresqlDatabase
 
 db = PostgresqlDatabase('postgres', user='postgres', password='yoh',
-                           host='localhost', port=5432)
+													 host='localhost', port=5432)
 # Connect to our database.
 db.connect()
 
-class lapd(Model):
-  # These are all the fields it has
-  # match up CharField/IntegerField/etc with correct type
+# class lapd(Model):
+# 	# These are all the fields it has
+# 	# match up CharField/IntegerField/etc with correct type
 
-  fid = CharField(primary_key=True) # primary key = unique id
-  desc_ = CharField()
-  slug = CharField()
-  home_addre = CharField()
-  x = DecimalField(null = True)
-  y = DecimalField(null = True)
-  
-  class Meta:
-    # data is coming from schools.db
-    database = db
-    # and it's in the table called 'events'
-    db_table = 'lapd_2016'
+# 	fid = CharField(primary_key=True) # primary key = unique id
+# 	desc_ = CharField()
+# 	slug = CharField()
+# 	home_addre = CharField()
+# 	sex = CharField()
+# 	x = DecimalField(null = True)
+# 	y = DecimalField(null = True)
+	
+# 	class Meta:
+# 		# data is coming from schools.db
+# 		database = db
+# 		# and it's in the table called 'events'
+# 		db_table = 'lapd_2016'
+
+
+class lasd(Model):
+	# These are all the fields it has
+	# match up CharField/IntegerField/etc with correct type
+
+	booking_nu = CharField(primary_key=True) # primary key = unique id
+	sex = CharField()
+	race = CharField()
+	charge = CharField()
+	charge_des = CharField()
+	charge_lev = CharField()
+	bail = CharField()
+	year = CharField()
+	longitude = DecimalField(null = True)
+	latitude = DecimalField(null = True)
+	slug = CharField()
+	
+	class Meta:
+		# data is coming from schools.db
+		database = db
+		# and it's in the table called 'events'
+		db_table = 'lasd_2012_2016'
 
 
